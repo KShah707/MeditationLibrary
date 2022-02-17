5160ed75e3728f37c7c4eb74f509072b43e9ecbd
KShah707 <kshah707@gmail.com>
2022-02-16 21:05:07 -0800

    Buttons to navigate down topic tree

--- app/src/main/java/com/kshah707/meditationlibrary/MainActivity.kt
+++ app/src/main/java/com/kshah707/meditationlibrary/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.kshah707.meditationlibrary
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -10,33 +13,74 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rootTopic = MeditationTopic("root", null, listOf(
+        // Define meditation topic tree
+        val rootTopic = MeditationTopic("root topic", null, listOf(
             MeditationTopic("Calm My Emotions", null, listOf(
-                MeditationTopic("from Work Stress", null, null, "google.com"),
-                MeditationTopic("from Social Situations", null, null, "google.com"),
+                MeditationTopic("from Work Stress", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
+                MeditationTopic("from Social Situations", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
             ), null),
             MeditationTopic("Narrow My Focus", null, listOf(
-                MeditationTopic("for One Hard Task", null, null, "google.com"),
-                MeditationTopic("for Many Easy Tasks", null, null, "google.com"),
+                MeditationTopic("for One Hard Task", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
+                MeditationTopic("for Many Easy Tasks", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
             ), null),
             MeditationTopic("Widen My Focus", null, listOf(
-                MeditationTopic("for Creativity", null, null, "google.com"),
-                MeditationTopic("for Playfulness", null, null, "google.com"),
+                MeditationTopic("for Creativity", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
+                MeditationTopic("for Playfulness", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
             ), null),
-            MeditationTopic("Try Something New", null, null, "youtube.com"),
+            MeditationTopic("Try Something New", null, listOf(), "https://www.youtube.com/watch?v=bzSTpdcs-EI"),
         ), null)
+
+        // set root as current topic
         var currentRoot = rootTopic
 
         // Populate starting layout
         val buttonLayout = findViewById<LinearLayout>(R.id.buttonLayout)
-        if (currentRoot.children.isNullOrEmpty()) {
-            // TODO handle case
-        } else {
-            for (topic in currentRoot.children!!) {
-                val newBtn = Button(this)
-                newBtn.text = topic.name
-                buttonLayout.addView(newBtn)
+        // Define onClick handler
+        fun btnOnClick(view: View?) {
+            val btn = view as Button
+
+            buttonLayout.removeAllViews()
+            if (currentRoot.children.isEmpty()) {
+                // FIXME Go to youtube link
+//                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(currentRoot.destinationUrl));
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                intent.setPackage("com.google.android.youtube");
+//                startActivity(intent)
+
+                // FIXME temp test, move back
+                // internally move up to parent
+                currentRoot = currentRoot.parent
+
+                // display parent's children as next level of navigation
+                for (child in currentRoot.children) {
+                    val newBtn = Button(this)
+                    newBtn.text = child.name
+                    newBtn.setOnClickListener { v -> btnOnClick(v) }
+                    buttonLayout.addView(newBtn)
+                }
+            } else {
+                // internally move down to a child
+                currentRoot = currentRoot.getChildByName(btn.text as String)!!
+
+                // display child's children as next level of navigation
+                for (child in currentRoot.children) {
+                    val newBtn = Button(this)
+                    newBtn.text = child.name
+                    newBtn.setOnClickListener { v -> btnOnClick(v) }
+                    buttonLayout.addView(newBtn)
+                }
             }
+        }
+
+        // populate starting layout with initial nodes
+        for (child in currentRoot.children) {
+            // create a Button to display the topic
+            val newBtn = Button(this)
+            newBtn.text = child.name
+
+            // set onClick
+            newBtn.setOnClickListener {v -> btnOnClick(v)}
+            buttonLayout.addView(newBtn)
         }
 
         // TODO:
@@ -55,19 +99,26 @@ class MainActivity : AppCompatActivity() {
 // can be nested
 class MeditationTopic(
     val name: String,
-    val imageUri: String?, // TODO make non nullable?
-    // either has children OR destinationUrl
-    var children: List<MeditationTopic>?,
+    val imageUri: String?,
+    var children: List<MeditationTopic>,
     var destinationUrl: String?
     )
 {
-    private var parent: MeditationTopic? = null
+    lateinit var parent: MeditationTopic
 
-    fun MeditationTopic() {
-        if (children.isNullOrEmpty()) {
-            for (child in children!!) {
-                child.parent = this
+    init {
+        for (child in children) {
+            child.parent = this
+        }
+    }
+
+    fun getChildByName(name: String): MeditationTopic? {
+        for (child in children) {
+            if (child.name == name) {
+                return child
             }
         }
+        // if no child matches
+        return null
     }
 }
